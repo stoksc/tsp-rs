9adb3331c52fa0cd73dcf916319bf40c64ebd50c
b <b@bs-MacBook-Pro.local>
2019-04-17 23:04:41 -0400

    change to use generics and 'metrizable' trait, so not bound to my shitty point struct

--- src/lib.rs
+++ src/lib.rs
@@ -5,6 +5,10 @@ use std::time;
 
 const MAX_ITER_WITHOUT_IMPR: u32 = 100;
 
+pub trait Metrizable {
+    fn distance(&self, y: &Self) -> f64;
+}
+
 #[derive(Debug, Clone, Copy, PartialEq)]
 pub struct Point {
     x: f64,
@@ -17,20 +21,28 @@ impl Point {
     }
 }
 
-#[derive(Debug, Clone, PartialEq)]
-pub struct Path {
-    pub len: f64,
-    pub path: Vec<Point>,
+impl Metrizable for Point {
+    fn distance(&self, b: &Point) -> f64 {
+        return ((self.x - b.x).powf(2.) + (self.y - b.y).powf(2.)).sqrt();
+    }
 }
 
-pub fn solve(v: &Vec<Point>, timeout: time::Duration) -> Path {
+#[derive(Debug, Clone, PartialEq)]
+pub struct Path<T: Metrizable> {
+    pub len: f64,
+    pub path: Vec<T>,
+}
+
+pub fn solve<T>(v: &Vec<T>, timeout: time::Duration) -> Path<T>
+where
+    T: Metrizable + Clone,
+{
     let mut result = v.clone();
     let mut iter_without_impr = 0;
     let mut past: f64 = std::f64::MAX;
     let start_time = time::Instant::now();
     loop {
-        let impr: Option<f64> = two_opt(&mut result);
-        match impr {
+        match two_opt(&mut result) {
             Some(x) => {
                 iter_without_impr = 0;
                 past = x;
@@ -49,12 +61,15 @@ pub fn solve(v: &Vec<Point>, timeout: time::Duration) -> Path {
     }
     Path {
         len: length(&result),
-        path: result,
+        path: result.to_vec(),
     }
 }
 
 #[inline]
-pub fn two_opt(v: &mut Vec<Point>) -> Option<f64> {
+pub fn two_opt<T>(v: &mut Vec<T>) -> Option<f64>
+where
+    T: Metrizable,
+{
     let p1: usize = rand::thread_rng().gen_range(0, v.len());
     let p2: usize = rand::thread_rng().gen_range(0, v.len());
 
@@ -64,6 +79,7 @@ pub fn two_opt(v: &mut Vec<Point>) -> Option<f64> {
 
     let prev_len = length(&v);
     v.swap(p1, p2);
+
     if length(&v) < prev_len {
         Some(prev_len - length(&v))
     } else {
@@ -73,7 +89,10 @@ pub fn two_opt(v: &mut Vec<Point>) -> Option<f64> {
 }
 
 #[inline]
-pub fn n_opt(n: usize, v: &mut Vec<Point>, l: f64) -> Option<f64> {
+pub fn n_opt<T>(n: usize, v: &mut Vec<T>, l: f64) -> Option<f64>
+where
+    T: Metrizable,
+{
     let s = v.len();
     let mut unq_points: HashSet<usize> = HashSet::new();
     while unq_points.len() < n {
@@ -96,7 +115,10 @@ pub fn n_opt(n: usize, v: &mut Vec<Point>, l: f64) -> Option<f64> {
 }
 
 #[inline]
-pub fn length(v: &Vec<Point>) -> f64 {
+pub fn length<T>(v: &Vec<T>) -> f64
+where
+    T: Metrizable,
+{
     if v.len() <= 0 {
         return 0.;
     }
@@ -104,13 +126,8 @@ pub fn length(v: &Vec<Point>) -> f64 {
     let mut sum = 0.;
     let mut prev = v.last().unwrap();
     for curr in v {
-        sum += euc_dist(&prev, curr);
+        sum += prev.distance(curr);
         prev = curr;
     }
     sum
-}
-
-#[inline]
-pub fn euc_dist(a: &Point, b: &Point) -> f64 {
-    return ((a.x - b.x).powf(2.) + (a.y - b.y).powf(2.)).sqrt();
 }
