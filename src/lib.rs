142ebbc67fb34e8201c7c3474a614d20b161c337
b <b@bs-MacBook-Pro.local>
2019-04-17 20:53:10 -0400

    improvements

--- src/lib.rs
+++ src/lib.rs
@@ -1,136 +1,116 @@
-extern crate itertools;
+use itertools::Itertools;
+use rand::Rng;
+use std::collections::HashSet;
+use std::time;
 
-#[cfg(test)]
-pub mod tests {
-    use crate::core::{Point};
-    use std::time::SystemTime;
+const MAX_ITER_WITHOUT_IMPR: u32 = 100;
 
-    #[test]
-    fn it_works() {
-        let filename = String::from("data/b52.tsp");
-        let mut v = crate::data::parse_file(&filename);
+#[derive(Debug, Clone, Copy, PartialEq)]
+pub struct Point {
+    x: f64,
+    y: f64,
+}
 
-        let t0 = SystemTime::now();
-        let (l, _sol): (f64, Vec<Point>) = crate::core::solve(&mut v);
-        println!("{:?}", SystemTime::now().duration_since(t0));
-        assert_eq!(l, 10000.);
+impl Point {
+    pub fn new(x: f64, y: f64) -> Point {
+        Point { x, y }
     }
 }
 
-pub mod core {
-    use std::hash::{Hash, Hasher};
-    use std::collections::HashSet;
-    use rand::Rng;
-    use itertools::Itertools;
+#[derive(Debug, Clone, PartialEq)]
+pub struct Path {
+    pub len: f64,
+    pub path: Vec<Point>,
+}
 
-    const MAX_ITER_WITHOUT_IMPR: u32 = 5;
-    const TOTAL_ITER: u32 = 100_000;
-
-    #[derive(Debug, Clone, Copy)]
-    pub struct Point(pub u32, pub f64, pub f64);
-
-    impl PartialEq for Point {
-        fn eq(&self, other: &Self) -> bool {
-            self.1 == other.1 && self.2 == other.2
-        }
-    }
-    
-    impl Eq for Point {}
-
-    impl Hash for Point {
-        fn hash<H: Hasher>(&self, state: &mut H) {
-            self.0.hash(state);
-        }
-    }
-
-    pub fn solve(v: &mut Vec<Point>) -> (f64, Vec<Point>) {
-        let mut iter_without_impr = 0;
-        let mut past: f64 = std::f64::MAX;
-        for _ in 1..TOTAL_ITER {
-            let impr: Option<f64> = n_opt(2, v, past);
-            match impr {
-                Some(x) => {
+pub fn solve(v: &Vec<Point>, timeout: time::Duration) -> Path {
+    let mut result = v.clone();
+    let mut iter_without_impr = 0;
+    let mut past: f64 = std::f64::MAX;
+    let start_time = time::Instant::now();
+    loop {
+        let impr: Option<f64> = two_opt(&mut result);
+        match impr {
+            Some(x) => {
+                iter_without_impr = 0;
+                past = x;
+            }
+            None => {
+                iter_without_impr += 1;
+                if iter_without_impr < MAX_ITER_WITHOUT_IMPR {
                     iter_without_impr = 0;
-                    past = x;
-                },
-                None => {
-                    iter_without_impr += 1;
-                    if iter_without_impr < MAX_ITER_WITHOUT_IMPR {
-                        iter_without_impr = 0;
-                        n_opt(4, v, past);
-                    }
+                    n_opt(4, &mut result, past);
                 }
             }
         }
-        (length(v), v.clone())
+        if start_time.elapsed() > timeout {
+            break;
+        }
     }
-
-    #[inline]
-    pub fn n_opt(n: usize, v: &mut Vec<Point>, l: f64) -> Option<f64> {
-        let s = v.len();
-        let mut unq_points: HashSet<usize> = HashSet::new();
-        while unq_points.len() < n {
-            let p: usize = rand::thread_rng().gen_range(0, s);
-            unq_points.insert(p);
-        }
-        for (p1, p2) in unq_points.iter().tuples() {
-            v.swap(*p1, *p2);
-        }
-        let nl: f64 = length(v);
-        if nl > l {
-            for (p1, p2) in unq_points.iter().tuples() {
-                v.swap(*p2, *p1);
-            }
-            return None;
-        }
-        return Some(nl)
-    }
-
-    #[inline]
-    pub fn length(v: & Vec<Point>) -> f64 {
-        if v.len() <= 0 {
-            return 0.
-        }
-
-        let mut sum = 0.;
-        let mut prev = v.last().unwrap();
-        for curr in v {
-            sum += euc_dist(&prev, curr);
-            prev = curr;
-        }
-        sum
-    }
-
-    #[inline]
-    pub fn euc_dist(a: &Point, b: &Point) -> f64 {
-        return ((a.1 - b.1).powf(2.) + (a.2 - b.2).powf(2.)).sqrt()
+    Path {
+        len: length(&result),
+        path: result,
     }
 }
 
-pub mod data {
-    use itertools::Itertools;
-    use crate::core::Point;
+#[inline]
+pub fn two_opt(v: &mut Vec<Point>) -> Option<f64> {
+    let p1: usize = rand::thread_rng().gen_range(0, v.len());
+    let p2: usize = rand::thread_rng().gen_range(0, v.len());
 
-    pub fn parse_file(filename: &String) -> Vec<Point> {
-        let file_err = "Something went wrong reading the file";
-        let int_parse_err = "Something went wrong parsing a point";
-        let contents = std::fs::read_to_string(filename).expect(file_err);
-        let mut d: Vec<Point> = Vec::new();
-        for chunk in &contents.split_whitespace().chunks(3) {
-            let mut p = Point(0, 0., 0.);
-            for (i, c) in chunk.enumerate() {
-                if c == "EOF" {
-                    return d;
-                }
-                match i {
-                    0 => p.0 = c.trim().parse().expect(int_parse_err),
-                    1 => p.1 = c.trim().parse().expect(int_parse_err),
-                    2 => p.2 = c.trim().parse().expect(int_parse_err),
-                    _ => continue,
-                }
-            }
-            d.push(p);
-        }
-        d
+    if p1 == p2 {
+        return None;
     }
+
+    let prev_len = length(&v);
+    v.swap(p1, p2);
+    if length(&v) < prev_len {
+        Some(prev_len - length(&v))
+    } else {
+        v.swap(p1, p2);
+        None
+    }
+}
+
+#[inline]
+pub fn n_opt(n: usize, v: &mut Vec<Point>, l: f64) -> Option<f64> {
+    let s = v.len();
+    let mut unq_points: HashSet<usize> = HashSet::new();
+    while unq_points.len() < n {
+        let p: usize = rand::thread_rng().gen_range(0, s);
+        unq_points.insert(p);
+    }
+    let mut swapped = Vec::new();
+    for (p1, p2) in unq_points.iter().tuples() {
+        swapped.push((p1.clone(), p2.clone()));
+        v.swap(*p1, *p2);
+    }
+    let nl: f64 = length(v);
+    if nl > l {
+        for (p1, p2) in swapped.iter().rev() {
+            v.swap(*p2, *p1);
+        }
+        return None;
+    }
+    return Some(nl);
+}
+
+#[inline]
+pub fn length(v: &Vec<Point>) -> f64 {
+    if v.len() <= 0 {
+        return 0.;
+    }
+
+    let mut sum = 0.;
+    let mut prev = v.last().unwrap();
+    for curr in v {
+        sum += euc_dist(&prev, curr);
+        prev = curr;
+    }
+    sum
+}
+
+#[inline]
+pub fn euc_dist(a: &Point, b: &Point) -> f64 {
+    return ((a.x - b.x).powf(2.) + (a.y - b.y).powf(2.)).sqrt();
 }
---
+++ tests/integration_test.rs
@@ -0,0 +1,45 @@
+use std::fs::File;
+use std::io::Read;
+use std::time;
+use tsp::Point;
+
+const TEST_DATA_FILENAME: &str = "tests/data/b52.tsp";
+const DEFAULT_TIMEOUT: u64 = 1;
+const EOF: &str = "EOF";
+
+#[test]
+fn test_solve() {
+    let filename = String::from(TEST_DATA_FILENAME);
+    let v = parse_tsp_file(&filename);
+
+    let timeout = time::Duration::from_secs(DEFAULT_TIMEOUT);
+    let solution = tsp::solve(&v, timeout);
+    println!(
+        "Solution to {} had length {}",
+        TEST_DATA_FILENAME, solution.len
+    );
+}
+
+fn parse_tsp_file(filename: &String) -> Vec<tsp::Point> {
+    let mut f = File::open(filename).expect("Failed to open test data file");
+    let mut contents = String::new();
+    f.read_to_string(&mut contents)
+        .expect("Failed to read file into string");
+
+    let mut results = Vec::new();
+
+    for line in contents.lines() {
+        if line.contains(EOF) {
+            break;
+        }
+
+        let mut words = line.split_whitespace().skip(1); //
+        let mut parse_next_float = || match words.next() {
+            Some(x) => x.trim().parse().expect("Failed to parse point"),
+            None => panic!("Not enough data to parse point"),
+        };
+        let point = Point::new(parse_next_float(), parse_next_float());
+        results.push(point);
+    }
+    results
+}
